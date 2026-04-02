71620626b2e4f34fd3b64e5af2d03a0404b47246
Joe Godwin <joe.godwin87@gmail.com>
2026-04-02 17:21:28 +0100

    Updated dashboard function to work out and pass through budget, total spend and amount remaining

--- app.py
+++ app.py
@@ -16,15 +16,17 @@ def index():
 #Pulls the data from the json file and then save the monthlyBudget amount to a variable to use in the html
 @app.route('/dashboard')
 def dashboard():
-  budget = load_data()
+  data = load_data()
+  transactions = data['transactions']
+  budget = data['monthlyBudget']
   total_spend = 0
-  for x in budget:
-    if x[1] == 'Debit':
-      total_spend += x[3]
+  for x in transactions:
+    if x['type'] == 'Debit':
+      total_spend += x['amount']
     else:
-      total_spend -= x[3]
+      total_spend -= x['amount']
   remaining = budget - total_spend
-  return render_template('index.html', budget=budget['monthlyBudget'], remaining=remaining)
+  return render_template('index.html', budget=budget, remaining=remaining, total_spend=total_spend)
 
 if __name__ == '__main__':
   app.run(debug=True, port=5001)
